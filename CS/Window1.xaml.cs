9982caa6c6dd422b2fb0623cf6e82c2a41b497b8
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Window1.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using DevExpress.Xpf.Grid;
+
+namespace CustomFiltering {
+
+    public partial class Window1 : Window {
+
+        public Window1() {
+            InitializeComponent();
+            grid.DataSource = new object[] { "A", "B", "C", "D", "E", "F", "G", "H" };
+        }
+
+        private void grid_CustomRowFilter(object sender, RowFilterEventArgs e) {
+            if (chkHideOdd.IsChecked.Value && e.ListSourceRowIndex % 2 == 1)
+                e.Visible = false;
+            if (chkHideEven.IsChecked.Value && e.ListSourceRowIndex % 2 == 0)
+                e.Visible = false;
+            e.Handled = !e.Visible ? true : false;
+        }
+
+        private void chk_CheckedOrUnchecked(object sender, RoutedEventArgs e) {
+            grid.RefreshData();
+        }
+    }
+}
---
+++ VB/Window1.xaml.vb
@@ -0,0 +1,29 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Imports DevExpress.Xpf.Grid
+
+Namespace CustomFiltering
+
+	Partial Public Class Window1
+		Inherits Window
+
+		Public Sub New()
+			InitializeComponent()
+			grid.DataSource = New Object() { "A", "B", "C", "D", "E", "F", "G", "H" }
+		End Sub
+
+		Private Sub grid_CustomRowFilter(ByVal sender As Object, ByVal e As RowFilterEventArgs)
+			If chkHideOdd.IsChecked.Value AndAlso e.ListSourceRowIndex Mod 2 = 1 Then
+				e.Visible = False
+			End If
+			If chkHideEven.IsChecked.Value AndAlso e.ListSourceRowIndex Mod 2 = 0 Then
+				e.Visible = False
+			End If
+			e.Handled = If((Not e.Visible), True, False)
+		End Sub
+
+		Private Sub chk_CheckedOrUnchecked(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			grid.RefreshData()
+		End Sub
+	End Class
+End Namespace
