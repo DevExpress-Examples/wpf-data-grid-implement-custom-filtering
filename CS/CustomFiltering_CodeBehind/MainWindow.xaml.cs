d5f3f7366b165286be97d5a3d80afcf1b6c65761
volkov.anton <anton.volkov@devexpress.com>
2021-05-13 18:06:27 +0300

    Update sample - use new Command API

---
+++ CS/CustomFiltering_CodeBehind/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using DevExpress.Xpf.Grid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomFiltering_CodeBehind {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            grid.ItemsSource = GetItems();
+        }
+        static IEnumerable<object> GetItems() {
+            return new object[] { "A", "B", "C", "D", "E", "F", "G", "H" };
+        }
+
+        void OnCustomRowFilter(object sender, RowFilterEventArgs e) {
+            if(chkHideOdd.IsChecked.Value && e.ListSourceRowIndex % 2 == 1)
+                e.Visible = false;
+            if(chkHideEven.IsChecked.Value && e.ListSourceRowIndex % 2 == 0)
+                e.Visible = false;
+            e.Handled = !e.Visible ? true : false;
+        }
+
+        void OnCheckedChanged(object sender, RoutedEventArgs e) {
+            grid.RefreshData();
+        }
+    }
+}
---
+++ CS/CustomFiltering_MVVM/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.DataAnnotations;
+using DevExpress.Mvvm.Xpf;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace CustomFiltering_MVVM {
+    public class MainViewModel : ViewModelBase {
+        public bool HideEvenItems {
+            get { return GetValue<bool>(nameof(HideEvenItems)); }
+            set { SetValue(value, UpdateFiltrationLogic, nameof(HideEvenItems)); }
+        }
+        public bool HideOddItems {
+            get { return GetValue<bool>(nameof(HideOddItems)); }
+            set { SetValue(value, UpdateFiltrationLogic, nameof(HideOddItems)); }
+        }
+        public ICommand<RowFilterArgs> CustomRowFilterCommand {
+            get { return GetValue<ICommand<RowFilterArgs>>(nameof(CustomRowFilterCommand)); }
+            set { SetValue(value, nameof(CustomRowFilterCommand)); }
+        }
+        public ObservableCollection<object> Items { get; }
+        public MainViewModel() {
+            Items = new ObservableCollection<object>(GetItems());
+            UpdateFiltrationLogic();
+        }
+        static IEnumerable<object> GetItems() {
+            return new object[] { "A", "B", "C", "D", "E", "F", "G", "H" };
+        }
+
+        void UpdateFiltrationLogic() {
+            CustomRowFilterCommand = new DelegateCommand<RowFilterArgs>(CustomRowFilter);
+        }
+
+        void CustomRowFilter(RowFilterArgs args) {
+            if(HideOddItems && args.SourceIndex % 2 == 1)
+                args.Visible = false;
+            if(HideEvenItems && args.SourceIndex % 2 == 0)
+                args.Visible = false;
+        }
+    }
+}
