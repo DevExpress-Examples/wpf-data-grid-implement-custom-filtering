8fb22e2ef43d87ea23b0f7a5150f12ef0740e98f
DevExpressExampleBot <fakeEmail>
2021-05-13 18:12:59 +0300

    Source auto update [skip ci]

---
+++ VB/CustomFiltering_CodeBehind/MainWindow.xaml.vb
@@ -0,0 +1,43 @@
+﻿Imports DevExpress.Xpf.Grid
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+
+Namespace CustomFiltering_CodeBehind
+	Partial Public Class MainWindow
+		Inherits Window
+
+		Public Sub New()
+			InitializeComponent()
+			grid.ItemsSource = GetItems()
+		End Sub
+		Private Shared Function GetItems() As IEnumerable(Of Object)
+			Return New Object() { "A", "B", "C", "D", "E", "F", "G", "H" }
+		End Function
+
+		Private Sub OnCustomRowFilter(ByVal sender As Object, ByVal e As RowFilterEventArgs)
+			If chkHideOdd.IsChecked.Value AndAlso e.ListSourceRowIndex Mod 2 = 1 Then
+				e.Visible = False
+			End If
+			If chkHideEven.IsChecked.Value AndAlso e.ListSourceRowIndex Mod 2 = 0 Then
+				e.Visible = False
+			End If
+			e.Handled = If(Not e.Visible, True, False)
+		End Sub
+
+		Private Sub OnCheckedChanged(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			grid.RefreshData()
+		End Sub
+	End Class
+End Namespace
---
+++ VB/CustomFiltering_MVVM/MainViewModel.vb
@@ -0,0 +1,59 @@
+﻿Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm.DataAnnotations
+Imports DevExpress.Mvvm.Xpf
+Imports System
+Imports System.Collections.Generic
+Imports System.Collections.ObjectModel
+Imports System.Linq
+
+Namespace CustomFiltering_MVVM
+	Public Class MainViewModel
+		Inherits ViewModelBase
+
+		Public Property HideEvenItems() As Boolean
+			Get
+				Return GetValue(Of Boolean)(NameOf(HideEvenItems))
+			End Get
+			Set(ByVal value As Boolean)
+				SetValue(value, AddressOf UpdateFiltrationLogic, NameOf(HideEvenItems))
+			End Set
+		End Property
+		Public Property HideOddItems() As Boolean
+			Get
+				Return GetValue(Of Boolean)(NameOf(HideOddItems))
+			End Get
+			Set(ByVal value As Boolean)
+				SetValue(value, AddressOf UpdateFiltrationLogic, NameOf(HideOddItems))
+			End Set
+		End Property
+		Public Property CustomRowFilterCommand() As ICommand(Of RowFilterArgs)
+			Get
+				Return GetValue(Of ICommand(Of RowFilterArgs))(NameOf(CustomRowFilterCommand))
+			End Get
+			Set(ByVal value As ICommand(Of RowFilterArgs))
+				SetValue(value, NameOf(CustomRowFilterCommand))
+			End Set
+		End Property
+		Public ReadOnly Property Items() As ObservableCollection(Of Object)
+		Public Sub New()
+			Items = New ObservableCollection(Of Object)(GetItems())
+			UpdateFiltrationLogic()
+		End Sub
+		Private Shared Function GetItems() As IEnumerable(Of Object)
+			Return New Object() { "A", "B", "C", "D", "E", "F", "G", "H" }
+		End Function
+
+		Private Sub UpdateFiltrationLogic()
+			CustomRowFilterCommand = New DelegateCommand(Of RowFilterArgs)(AddressOf CustomRowFilter)
+		End Sub
+
+		Private Sub CustomRowFilter(ByVal args As RowFilterArgs)
+			If HideOddItems AndAlso args.SourceIndex Mod 2 = 1 Then
+				args.Visible = False
+			End If
+			If HideEvenItems AndAlso args.SourceIndex Mod 2 = 0 Then
+				args.Visible = False
+			End If
+		End Sub
+	End Class
+End Namespace
