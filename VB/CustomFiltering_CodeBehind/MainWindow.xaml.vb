925066f78ef2bd3cb6860260457ee44b24358b05
DevExpressExampleBot <fakeEmail>
2022-07-26 10:36:02 +0400

    Source auto update [skip ci]

--- VB/CustomFiltering_CodeBehind/MainWindow.xaml.vb
+++ VB/CustomFiltering_CodeBehind/MainWindow.xaml.vb
@@ -1,43 +1,30 @@
-﻿Imports DevExpress.Xpf.Grid
-Imports System
+Imports DevExpress.Xpf.Grid
 Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 
 Namespace CustomFiltering_CodeBehind
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-			grid.ItemsSource = GetItems()
-		End Sub
-		Private Shared Function GetItems() As IEnumerable(Of Object)
-			Return New Object() { "A", "B", "C", "D", "E", "F", "G", "H" }
-		End Function
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub OnCustomRowFilter(ByVal sender As Object, ByVal e As RowFilterEventArgs)
-			If chkHideOdd.IsChecked.Value AndAlso e.ListSourceRowIndex Mod 2 = 1 Then
-				e.Visible = False
-			End If
-			If chkHideEven.IsChecked.Value AndAlso e.ListSourceRowIndex Mod 2 = 0 Then
-				e.Visible = False
-			End If
-			e.Handled = If(Not e.Visible, True, False)
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.grid.ItemsSource = GetItems()
+        End Sub
 
-		Private Sub OnCheckedChanged(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			grid.RefreshData()
-		End Sub
-	End Class
+        Private Shared Function GetItems() As IEnumerable(Of Object)
+            Return New Object() {"A", "B", "C", "D", "E", "F", "G", "H"}
+        End Function
+
+        Private Sub OnCustomRowFilter(ByVal sender As Object, ByVal e As RowFilterEventArgs)
+            If Me.chkHideOdd.IsChecked.Value AndAlso e.ListSourceRowIndex Mod 2 = 1 Then e.Visible = False
+            If Me.chkHideEven.IsChecked.Value AndAlso e.ListSourceRowIndex Mod 2 = 0 Then e.Visible = False
+            e.Handled = If(Not e.Visible, True, False)
+        End Sub
+
+        Private Sub OnCheckedChanged(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Me.grid.RefreshData()
+        End Sub
+    End Class
 End Namespace
