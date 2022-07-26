ebbfd080dee74d3c7d94dbdb3a810768c88d45d2
DevExpressExampleBot <fakeEmail>
2022-07-26 10:31:57 +0400

    Source auto update [skip ci]

---
+++ VB/App.xaml.vb
@@ -0,0 +1,12 @@
+Imports System.Windows
+
+Namespace CustomFiltering
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
+End Namespace
--- VB/Window1.xaml.vb
+++ VB/Window1.xaml.vb
@@ -1,29 +1,24 @@
-﻿Imports Microsoft.VisualBasic
 Imports System.Windows
 Imports DevExpress.Xpf.Grid
 
 Namespace CustomFiltering
 
-	Partial Public Class Window1
-		Inherits Window
+    Public Partial Class Window1
+        Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-			grid.ItemsSource = New Object() { "A", "B", "C", "D", "E", "F", "G", "H" }
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.grid.ItemsSource = New Object() {"A", "B", "C", "D", "E", "F", "G", "H"}
+        End Sub
 
-		Private Sub grid_CustomRowFilter(ByVal sender As Object, ByVal e As RowFilterEventArgs)
-			If chkHideOdd.IsChecked.Value AndAlso e.ListSourceRowIndex Mod 2 = 1 Then
-				e.Visible = False
-			End If
-			If chkHideEven.IsChecked.Value AndAlso e.ListSourceRowIndex Mod 2 = 0 Then
-				e.Visible = False
-			End If
-			e.Handled = If((Not e.Visible), True, False)
-		End Sub
+        Private Sub grid_CustomRowFilter(ByVal sender As Object, ByVal e As RowFilterEventArgs)
+            If Me.chkHideOdd.IsChecked.Value AndAlso e.ListSourceRowIndex Mod 2 = 1 Then e.Visible = False
+            If Me.chkHideEven.IsChecked.Value AndAlso e.ListSourceRowIndex Mod 2 = 0 Then e.Visible = False
+            e.Handled = If(Not e.Visible, True, False)
+        End Sub
 
-		Private Sub chk_CheckedOrUnchecked(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			grid.RefreshData()
-		End Sub
-	End Class
+        Private Sub chk_CheckedOrUnchecked(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Me.grid.RefreshData()
+        End Sub
+    End Class
 End Namespace
